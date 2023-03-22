53923cbabea3f7442e1cf95e2a2ccb3bc9fd794b
Karnveer Grewal <karnveergrewal4@gmail.com>
2023-03-22 12:08:14 -0400

    added Read More button to tour info

--- src/components/ToursItem.js
+++ src/components/ToursItem.js
@@ -1,5 +1,8 @@
+import { useState } from 'react';
+
 const TourItem = (props) => {
-  console.log(props);
+  const [readMore, setReadMore] = useState(false);
+
   return (
     <div className='tours-item'>
       <img src={props.image} alt={props.name} className='tours-img' />
@@ -8,7 +11,15 @@ const TourItem = (props) => {
         <p className='tours-price'>${props.price}</p>
       </div>
 
-      <p className='tours-info'>{props.info}</p>
+      <p className='tours-info'>
+        {readMore ? props.info : `${props.info.substring(0, 200)}...`}
+        <button
+          className='tours-read-more'
+          onClick={() => setReadMore((prevReadMore) => !prevReadMore)}
+        >
+          {readMore ? 'Show Less' : 'Read More'}
+        </button>
+      </p>
       <button
         className='tours-button'
         onClick={() => props.deleteTour(props.id)}
